28fd44e758cee5dd6f3d17b4cffeee4bb938d821
Dan Berenholtz <dan@whowentout.com>
2014-10-11 03:55:01 -0400

    some more changes

---
+++ AudioDemo/YapsViewController.h
@@ -0,0 +1,13 @@
+//
+//  YapsViewController.h
+//  AudioDemo
+//
+//  Created by Dan Berenholtz on 9/7/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YapsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ AudioDemo/YapsViewController.m
@@ -0,0 +1,81 @@
+//
+//  ContactsViewController.m
+//  AudioDemo
+//
+//  Created by Dan Berenholtz on 9/7/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import "YapsViewController.h"
+#import "MBProgressHUD.h"
+
+
+@interface YapsViewController ()
+
+@end
+
+static NSString *CellIdentifier = @"Cell";
+
+@implementation YapsViewController
+{
+    NSArray *yaps;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"Your Yaps";
+    
+    // TEXT COLOR OF UINAVBAR
+    [self.navigationController.navigationBar
+     setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
+    
+    self.navigationController.navigationBar.tintColor = THEME_RED_COLOR;
+    
+    self.navigationItem.hidesBackButton = YES;
+    
+    yaps = [NSArray arrayWithObjects:@"Yap1", @"Yap2", @"Yap3", @"Yap4", @"Yap5", @"Yap6", @"Yap7", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma UITableViewDataSource
+- (int) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [yaps count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [yaps objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"airplane.png"];
+    cell.detailTextLabel.text = @"Reply";
+    
+    return cell;
+}
+
+@end
