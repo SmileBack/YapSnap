fa9a5feca57e3af7b3390f93bda501986ff7f2e8
danberenholtz <dan@whowentout.com>
2015-01-20 01:42:47 -0500

    commented out line of code which was being called excessively

--- AudioDemo/AudioCaptureViewController.m
+++ AudioDemo/AudioCaptureViewController.m
@@ -165,7 +165,7 @@ static const float TIMER_INTERVAL = .01;
     
     if (self.elapsedTime >= MAX_CAPTURE_TIME) {
         [timer invalidate];
-        [self performSegueWithIdentifier:@"Prepare Yap For Text Segue" sender:nil];
+        //[self performSegueWithIdentifier:@"Prepare Yap For Text Segue" sender:nil];
         [self.audioSource stopAudioCapture:self.elapsedTime];
     }
 }
