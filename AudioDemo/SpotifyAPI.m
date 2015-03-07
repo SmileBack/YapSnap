8d252fe8de9c70afac57cd454211c866e14cfa87
Jon Deokule <jdeokule@gmail.com>
2015-03-07 14:19:18 -0500

    Spotify token

--- AudioDemo/SpotifyAPI.m
+++ AudioDemo/SpotifyAPI.m
@@ -8,8 +8,14 @@
 
 #import "SpotifyAPI.h"
 #import "YSTrack.h"
+#import "Environment.h"
 
 #define API_KEY @""
+
+@interface SpotifyAPI()
+@property (nonatomic, strong) NSString *tokenType;
+@property (nonatomic, strong) NSString *token;
+@end
 
 @implementation SpotifyAPI
 
@@ -19,27 +25,60 @@ static SpotifyAPI *sharedInstance;
 {
     if (!sharedInstance) {
         sharedInstance = [SpotifyAPI new];
+        [sharedInstance getAccessToken];
     }
 
     return sharedInstance;
 }
 
+- (void) getAccessToken
+{
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+
+    NSString *value = [NSString stringWithFormat:@"Basic %@", [Environment sharedInstance].spotifyToken];
+    [manager.requestSerializer setValue:value forHTTPHeaderField:@"Authorization"];
+
+    __weak SpotifyAPI *weakSelf = self;
+    [manager POST:@"https://accounts.spotify.com/api/token"
+       parameters:@{@"grant_type": @"client_credentials"}
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+              NSLog(@"Response: %@", responseObject);
+              if ([responseObject isKindOfClass:[NSDictionary class]]) {
+                  NSDictionary *response = responseObject;
+                  weakSelf.tokenType = response[@"token_type"];
+                  weakSelf.token = response[@"access_token"];
+              }
+          }
+          failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+              NSLog(@"Error: %@", error);
+          }];
+}
+
+- (void) setAuthorizationOnManager:(AFHTTPRequestOperationManager *)manager
+{
+    if (self.token && self.tokenType) {
+        NSString *value = [NSString stringWithFormat:@"%@ %@", self.tokenType, self.token];
+        [manager.requestSerializer setValue:value forHTTPHeaderField:@"Authorization"];
+    } else {
+        NSLog(@"NO TOKEN!!");
+    }
+}
+
 - (void) searchSongs:(NSString *)searchString withCallback:(SpotifySongCallback)callback
 {
     NSString *url = @"https://api.spotify.com/v1/search";
-    
+
     NSDictionary *params = @{@"q": searchString,
                              @"type": @"track",
                              @"limit": @50};
 
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [self setAuthorizationOnManager:manager];
     [manager GET:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(@"JSON: %@", responseObject);
         NSDictionary *response = responseObject;
         NSArray *items = response[@"tracks"][@"items"];
-        
+
         NSArray *songs = [YSTrack tracksFromDictionaryArray:items];
-        NSLog(@"about to make callback with songs: %@", songs);
         callback(songs, nil);
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
