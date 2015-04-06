e45a9f96438efdca193844c7f983f46a54d2b013
danberenholtz <dan@whowentout.com>
2015-04-06 16:10:02 -0400

    polish

--- AudioDemo/AddTextViewController.m
+++ AudioDemo/AddTextViewController.m
@@ -82,7 +82,7 @@
         self.contactLabel.text = @"";
     }
     
-    double delay = 0.5;
+    double delay = 0.2;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.continueButton startToPulsate];
     });
