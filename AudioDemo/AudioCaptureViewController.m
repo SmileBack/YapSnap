067c08efdd87b6447cff63c489718190f560bd5a
danberenholtz <dan@whowentout.com>
2015-04-21 19:45:04 -0400

    polish

--- AudioDemo/AudioCaptureViewController.m
+++ AudioDemo/AudioCaptureViewController.m
@@ -473,7 +473,7 @@ static const float TIMER_INTERVAL = .01;
         if (!self.didTapMicModeButtonForFirstTime) {
             double delay = .1;
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [[YTNotifications sharedNotifications] showModeText:@"Mic Mode"];
+                //[[YTNotifications sharedNotifications] showModeText:@"Mic Mode"];
                 [[NSUserDefaults standardUserDefaults] setBool:YES forKey:TAPPED_MIC_MODE_BUTTON_FOR_FIRST_TIME_KEY];
             });
         }
