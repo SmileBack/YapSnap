1e1ab8f3af76ae8ab8ac4e9bd45c04ccc4183310
danberenholtz <dan@whowentout.com>
2015-04-21 02:23:24 -0400

    minor polish

--- AudioDemo/YSSpotifySourceController.m
+++ AudioDemo/YSSpotifySourceController.m
@@ -259,6 +259,7 @@
             [self.loadingIndicator stopAnimating];
             self.musicIcon.hidden = NO;
             self.carousel.hidden = YES;
+            self.smallMusicIcon.hidden = YES;
             
             if ([self internetIsNotReachable]) {
                 double delay = 0.1;
