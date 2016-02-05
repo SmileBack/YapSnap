b7705b673cb6dfc2e77933619ca175093d4b54d8
Rudd T <rudd@varka.la>
2016-02-05 14:08:55 -0500

    no results on giphy controller

--- AudioDemo/GiphySelectionViewController.m
+++ AudioDemo/GiphySelectionViewController.m
@@ -12,11 +12,18 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import <Giphy-iOS/AXCGiphy.h>
 
+typedef NS_ENUM(NSInteger, GiphySection) {
+    GiphySectionLoading = 0,
+    GiphySectionGifs,
+    GiphySectionEmpty
+};
+
 @interface GiphySelectionViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate>
 
 @property (strong, nonatomic) UICollectionView *collectionView;
 @property (strong) NSArray *gifs;
 @property (strong) UISearchBar *searchBar;
+@property BOOL loading;
 
 @end
 
@@ -25,13 +32,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [AXCGiphy setGiphyAPIKey:kGiphyPublicAPIKey];
-    [AXCGiphy searchGiphyWithTerm:self.searchTerm limit:10 offset:0 completion:^(NSArray *results, NSError *error) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.gifs = results;
-            self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout appLayout];
-            [self.collectionView reloadData];
-        });
-    }];
+    [self searchForTerm];
     
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Dismiss" style:UIBarButtonItemStylePlain target:self action:@selector(didTapDismiss)];
@@ -60,7 +61,7 @@
 #pragma mark - UICollectionViewDelegate
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 0) {
+    if (indexPath.section == GiphySectionLoading) {
         UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Loading" forIndexPath:indexPath];
         [[cell viewWithTag:888] removeFromSuperview];
         UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
@@ -73,19 +74,39 @@
                                ]];
         [spinner startAnimating];
         return cell;
-    } else {
+    } else if (indexPath.section == GiphySectionGifs) {
         GifCell *cell = (GifCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"Gif" forIndexPath:indexPath];
         AXCGiphy * gif = self.gifs[indexPath.item];
         [cell.gifView sd_setImageWithURL:gif.originalImage.url];
+        return cell;
+    } else {
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Loading" forIndexPath:indexPath];
+        [[cell viewWithTag:888] removeFromSuperview];
+        UILabel *label = UILabel.new;
+        label.text = @"No results found.\nTap above to search for gifs.";
+        label.numberOfLines = 0;
+        label.font = [UIFont fontWithName:@"Futura-Medium" size:20];
+        label.adjustsFontSizeToFitWidth = YES;
+        label.textColor = UIColor.whiteColor;
+        label.textAlignment = NSTextAlignmentCenter;
+        label.tag = 888;
+        [cell addSubview:label];
+        label.translatesAutoresizingMaskIntoConstraints = NO;
+        [cell addConstraints:@[
+                               [NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:cell attribute:NSLayoutAttributeCenterX multiplier:1 constant:0],
+                               [NSLayoutConstraint constraintWithItem:label attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:cell attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]
+                               ]];
         return cell;
     }
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    if (section == 0) {
-        return self.gifs ? 0 : 1;
-    } else {
+    if (section == GiphySectionLoading) {
+        return self.loading ? 1 : 0;
+    } else if (section == GiphySectionGifs) {
         return self.gifs.count;
+    } else {
+        return self.gifs.count == 0 ? 1 : 0;
     }
 }
 
@@ -96,20 +117,26 @@
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 2;
+    return 3;
 }
 
 #pragma mark - UISearchBarDelegate
 
 -(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     self.gifs = nil;
-    self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout screenWidthLayout];
     self.searchTerm = searchBar.text;
+    [self searchForTerm];
+}
+
+- (void)searchForTerm {
+    //    self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout screenWidthLayout];
+    self.loading = YES;
     [self.collectionView reloadData];
     [AXCGiphy searchGiphyWithTerm:self.searchTerm limit:10 offset:0 completion:^(NSArray *results, NSError *error) {
+        self.loading = NO;
         dispatch_async(dispatch_get_main_queue(), ^{
             self.gifs = results;
-            self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout appLayout];
+            //            self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout appLayout];
             [self.collectionView reloadData];
         });
     }];
