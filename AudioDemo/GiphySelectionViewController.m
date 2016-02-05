d7230b0cd82864a8547d286a4e642883dfea3965
Rudd T <rudd@varka.la>
2016-02-05 12:11:56 -0500

    search works on giphy, loading spinner

--- AudioDemo/GiphySelectionViewController.m
+++ AudioDemo/GiphySelectionViewController.m
@@ -12,7 +12,7 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import <Giphy-iOS/AXCGiphy.h>
 
-@interface GiphySelectionViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@interface GiphySelectionViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate>
 
 @property (strong, nonatomic) UICollectionView *collectionView;
 @property (strong) NSArray *gifs;
@@ -26,10 +26,11 @@
     [super viewDidLoad];
     [AXCGiphy setGiphyAPIKey:kGiphyPublicAPIKey];
     [AXCGiphy searchGiphyWithTerm:self.searchTerm limit:10 offset:0 completion:^(NSArray *results, NSError *error) {
-        self.gifs = results;
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.gifs = results;
+            self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout appLayout];
             [self.collectionView reloadData];
-        }];
+        });
     }];
     
     self.automaticallyAdjustsScrollViewInsets = NO;
@@ -37,10 +38,13 @@
     self.navigationItem.title = @"Select a Gif";
     
     self.searchBar = [[UISearchBar alloc] init];
+    self.searchBar.delegate = self;
+    self.searchBar.text = self.searchTerm;
     
-    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[UICollectionViewFlowLayout appLayout]];
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:[UICollectionViewFlowLayout screenWidthLayout]];
     self.collectionView.backgroundColor = THEME_BACKGROUND_COLOR;
     [self.collectionView registerClass:[GifCell class] forCellWithReuseIdentifier:@"Gif"];
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Loading"];
     self.collectionView.dataSource = self;
     self.collectionView.delegate = self;
     
@@ -56,14 +60,33 @@
 #pragma mark - UICollectionViewDelegate
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    GifCell *cell = (GifCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"Gif" forIndexPath:indexPath];
-    AXCGiphy * gif = self.gifs[indexPath.item];
-    [cell.gifView sd_setImageWithURL:gif.originalImage.url];
-    return cell;
+    if (indexPath.section == 0) {
+        UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Loading" forIndexPath:indexPath];
+        [[cell viewWithTag:888] removeFromSuperview];
+        UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        spinner.tag = 888;
+        [cell addSubview:spinner];
+        spinner.translatesAutoresizingMaskIntoConstraints = NO;
+        [cell addConstraints:@[
+                               [NSLayoutConstraint constraintWithItem:spinner attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:cell attribute:NSLayoutAttributeCenterX multiplier:1 constant:0],
+                               [NSLayoutConstraint constraintWithItem:spinner attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:cell attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]
+                               ]];
+        [spinner startAnimating];
+        return cell;
+    } else {
+        GifCell *cell = (GifCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"Gif" forIndexPath:indexPath];
+        AXCGiphy * gif = self.gifs[indexPath.item];
+        [cell.gifView sd_setImageWithURL:gif.originalImage.url];
+        return cell;
+    }
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.gifs.count;
+    if (section == 0) {
+        return self.gifs ? 0 : 1;
+    } else {
+        return self.gifs.count;
+    }
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -73,7 +96,22 @@
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 1;
+    return 2;
+}
+
+#pragma mark - UISearchBarDelegate
+
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    self.gifs = nil;
+    self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout screenWidthLayout];
+    [self.collectionView reloadData];
+    [AXCGiphy searchGiphyWithTerm:self.searchTerm limit:10 offset:0 completion:^(NSArray *results, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.gifs = results;
+            self.collectionView.collectionViewLayout = [UICollectionViewFlowLayout appLayout];
+            [self.collectionView reloadData];
+        });
+    }];
 }
 
 #pragma mark - Actions
